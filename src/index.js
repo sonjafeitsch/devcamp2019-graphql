6e9767946de6bf3fcc4e1bf8c347023837bf4b20
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2019-03-22 15:01:06 +0100

    Add some mutations

--- src/index.js
+++ src/index.js
@@ -7,21 +7,35 @@ const links = [{
     description: 'Awesome webpage'
 }]
 
-
+let idCount = links.length;
 const resolvers = {
     Query: {
         info: () => 'This is an API',
-        feed: () => links
+        feed: () => links,
+        link: (parent, args) => {
+            console.log(args)
+            var filteredLinks = links.filter(a => {
+                return a.id === args.id
+            })
+            console.log(filteredLinks)
+            return filteredLinks;
+        }
     },
-    Link: {
-        id: (parent) => parent.id,
-        url: (parent) => parent.url,
-        description: (parent) => parent.description 
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount}`,
+                url: args.url,
+                description: args.description
+            }
+            links.push(link)
+            return link
+        }
     }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 server.start(() => console.log("Server is running at http://localhost:4000"))
