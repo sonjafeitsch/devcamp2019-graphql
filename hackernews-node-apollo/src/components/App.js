43ff3d528fed29d9cc5be6c631dd0f936ddd4c06
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2019-04-01 18:06:28 +0200

    Add second propject for adding the frontend

---
+++ hackernews-node-apollo/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '../styles/App.css';
+import User from './User';
+
+class App extends Component {
+  render() {
+    return (
+      <User />
+    );
+  }
+}
+
+export default App;
---
+++ hackernews-node-apollo/src/components/User.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const USER_QUERY = gql`
+    {
+        users {
+            name
+        }
+    }
+`
+
+class User extends Component {
+    render(){
+        // const userNames = [
+        //     {
+        //         name: "Bob"
+        //     },
+        //     {
+        //         name: "Alice"
+        //     },
+        //   ]
+        return (
+            <Query query={USER_QUERY}>
+                {({loading, error, data}) => {
+                    if(loading) return <div>Fetching</div>
+                    if(error) return <div>error</div>
+                    return (
+                        <div>{data.users.map(name => <Element name={name.name} />)}</div>
+                    )
+                }}
+            </Query>
+            // <div>{userNames.map(name => <Element name={name.name} />)}</div>
+        )
+    }
+}
+
+function Element(props){
+    return <div>{props.name}</div>
+}
+
+export default User
