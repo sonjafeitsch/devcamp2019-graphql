bd0bef22e7007f16b3e07ac613df00e6c3ea74ac
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2019-04-03 10:47:55 +0200

    Setup basic project for livedemo

---
+++ hackernews-live-demo/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '../styles/App.css';
+import User from './User';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello GrahQL!</h1>
+    );
+  }
+}
+
+export default App;
