844326879e65427fbbca9da7bde75fbb0f13b091
Sonja Hohlfeld <sonja.hohlfeld@mayflower.de>
2019-04-04 11:29:16 +0200

    Final basic setup

--- hackernews-live-demo/src/components/App.js
+++ hackernews-live-demo/src/components/App.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
-import '../styles/App.css';
-import User from './User';
 
 class App extends Component {
   render () {
-    return <User />;
+    return <div>Hello GraphQL</div>;
   }
 }
 
